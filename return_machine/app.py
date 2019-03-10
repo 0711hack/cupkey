37cfb089a826197d782a6c3b72cddc1019ad1733
Ben Lebherz <git@benleb.de>
2019-03-10 07:40:04 +0100

    add return machine flask app

--- return_machine/app.py
+++ return_machine/app.py
@@ -71,4 +71,4 @@ api.add_resource(VerifyCupSignature, '/cup/verify')
 api.add_resource(ShopKeys, '/keys/shop')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
